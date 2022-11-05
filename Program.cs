72c893e6d0121ad28e6db1041306c83e8793dcbe
Eseoghene Okoro <enerhen2013@gmail.com>
2022-11-05 15:02:11 +0100

    enabled swagger

--- Program.cs
+++ Program.cs
@@ -18,11 +18,10 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
